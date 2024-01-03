2743288f617d38121963e8f514990a5d8b021dd4
VugluskrDevNull <74187607+VugluskrDevNull@users.noreply.github.com>
2024-01-03 14:14:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
-#include "qircbot.h"
-#include "sett.h"
-#include <QApplication>
+#include <QCoreApplication>
+#include <QSettings>
+#include <QFile>
+#include <QDebug>
+#include <QString>
+#include <QTextStream>
+#include <QTimer>
+#include <QTime>
 
-int main(int argc, char *argv[])
+#include "console.h"
+//#include "Interface.h"        //
+#include <timebomb.h>
+
+int main(int argc, char** argv)
 {
-    QApplication a(argc, argv);
-    Sett  s;
-    Bot * bot = new Bot;
+#ifndef Q_OS_WIN32
+  setlocale(LC_ALL, "");
+#endif
+    QCoreApplication app(argc, argv);
 
-    if (!(bot->connect()))
-        return  1;
-    s.show();
-    return a.exec();
+    qDebug()<<"enter !bomb\n";
+    Console *cons = new Console();
+    GameTimeBomb * game = new GameTimeBomb(cons);
+
+    cons->run();
+    QObject::connect(cons, SIGNAL (userInput(QString)), game,  SLOT(userInput(QString)));    //  запуск игры офлайн
+    QObject::connect(game, SIGNAL (send(QString)), cons,  SLOT(send(QString)));
+    QObject::connect(cons, SIGNAL(quit()), &app, SLOT(quit()));
+    QObject::connect(cons, SIGNAL(quit()), game, SLOT(quit()));
+    return app.exec();
 }
+
 
